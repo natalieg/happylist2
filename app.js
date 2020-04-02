22601464bae79899a6abc69a8ae8d32b1c6eabcb
Natalie <natalie.gornicki@gmail.com>
2020-04-02 19:59:09 +0200

    heroku

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/api', apiRouter);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("frontend/build"))
     app.get("*",(req, res) => {
-        res.sendfile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
